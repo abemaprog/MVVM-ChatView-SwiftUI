5925dd7eedc1681b119ff1282744ce21739e1d10
abema <appdev.abe@gmail.com>
2024-09-10 22:08:14 +0900

    Initial Commit

---
+++ ChatViewWithSwiftUI/ChatViewWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatViewWithSwiftUIApp.swift
+//  ChatViewWithSwiftUI
+//
+//  Created by Manato Abe on 2024/09/10.
+//
+
+import SwiftUI
+
+@main
+struct ChatViewWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatViewWithSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChatViewWithSwiftUI
+//
+//  Created by Manato Abe on 2024/09/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
